a478eb6f53ded49c3691e5ce1d59990e257b2413
villivald <maxim.villivald@gmail.com>
2021-09-26 01:20:58 +0300

    add delete to api

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -30,16 +30,25 @@ export default async function handler(req, res) {
   switch (method) {
     case "GET":
       try {
-        const paikat = await Todo.find({});
-        res.status(200).json({ success: true, data: paikat });
+        const todos = await Todo.find({});
+        res.status(200).json({ success: true, data: todos });
       } catch (error) {
         res.status(400).json({ success: false });
       }
       break;
     case "POST":
       try {
-        const court = await Todo.create(req.body);
-        res.status(201).json({ success: true, data: court });
+        const todo = await Todo.create(req.body);
+        res.status(201).json({ success: true, data: todo });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "DELETE":
+      try {
+        const todo = await Todo.find(req.body);
+        await Todo.findByIdAndRemove(request.body.id);
+        res.status(204).json({ success: true, data: todo });
       } catch (error) {
         res.status(400).json({ success: false });
       }
