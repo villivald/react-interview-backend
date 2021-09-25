6f795d0a674ab8a48fd59e95a2256af259306eca
villivald <maxim.villivald@gmail.com>
2021-09-25 14:02:01 +0300

    add models & modify dbconnect

---
+++ models/Todo.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+  },
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  complete: {
+    type: Boolean,
+  },
+  deleted: {
+    type: Boolean,
+  },
+  important: {
+    type: Boolean,
+  },
+});
+
+export default mongoose.models.Todo || mongoose.model("Todo", TodoSchema);
--- pages/api/todos.js
+++ pages/api/todos.js
@@ -1,15 +1,24 @@
-import { connectToDatabase } from "../../util/mongodb";
+import dbConnect from "../../util/dbConnect";
+import Todo from "../../models/Todo";
 
 export default async function handler(req, res) {
   const { method } = req;
 
-  const { db } = await connectToDatabase();
+  await dbConnect();
 
   switch (method) {
     case "GET":
       try {
-        const todos = await db.collection("todos").find({}).toArray();
-        res.status(200).json({ success: true, data: todos });
+        const paikat = await Todo.find({});
+        res.status(200).json({ success: true, data: paikat });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const court = await Todo.create(req.body);
+        res.status(201).json({ success: true, data: court });
       } catch (error) {
         res.status(400).json({ success: false });
       }
