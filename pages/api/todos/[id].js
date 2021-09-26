811fdecd1bea136d60d403aad5543487f570cd76
villivald <maxim.villivald@gmail.com>
2021-09-26 12:22:05 +0300

    fix api structure & add [id]

--- pages/api/todos/[id].js
+++ pages/api/todos/[id].js
@@ -1,5 +1,5 @@
-import dbConnect from "../../util/dbConnect";
-import Todo from "../../models/Todo";
+import dbConnect from "../../../util/dbConnect";
+import Todo from "../../../models/Todo";
 import Cors from "cors";
 
 const initMiddleware = (middleware) => {
@@ -16,45 +16,47 @@ const initMiddleware = (middleware) => {
 
 const cors = initMiddleware(
   Cors({
-    methods: ["GET", "POST", "OPTIONS", "DELETE"],
+    methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"],
   })
 );
 
 export default async function handler(req, res) {
   await cors(req, res);
 
-  const { method } = req;
+  const {
+    query: { id },
+    method,
+  } = req;
 
   await dbConnect();
 
   switch (method) {
     case "GET":
       try {
-        const todos = await Todo.find({});
-        res.status(200).json({ success: true, data: todos });
+        const todo = await Todo.findById(id);
+        res.status(200).json({ success: true, data: todo });
       } catch (error) {
         res.status(400).json({ success: false });
       }
       break;
-    case "POST":
+    case "PUT":
       try {
-        const todo = await Todo.create(req.body);
-        res.status(201).json({ success: true, data: todo });
+        const todo = await Todo.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        res.status(200).json({ success: true, data: todo });
       } catch (error) {
         res.status(400).json({ success: false });
       }
       break;
     case "DELETE":
       try {
-        const todo = await Todo.find(req.body);
-        await Todo.findByIdAndRemove(request.body.id);
-        res.status(204).json({ success: true, data: todo });
+        await Todo.deleteOne({ _id: id });
+        res.status(200).json({ success: true, data: {} });
       } catch (error) {
         res.status(400).json({ success: false });
       }
-      break;
-    default:
-      res.status(400).json({ success: false });
       break;
   }
 }
